adca301a1e7da834c764f0a08a0076f544522b98
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2024-03-07 20:34:24 +0100

    meglio - da finire

--- diem_turtlebot_ws/src/nav_pkg/nav_pkg/nav_step.py
+++ diem_turtlebot_ws/src/nav_pkg/nav_pkg/nav_step.py
@@ -3,13 +3,12 @@ from rclpy.node import Node
 from rclpy.logging import LoggingSeverity
 from geometry_msgs.msg import Twist
 from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
+from rclpy.clock import Duration
 
 class Handler(Node):
     def __init__(self):
         super().__init__("obstacle_avoidance_node") #Init node
         self.get_logger().set_level(LoggingSeverity.DEBUG)
-        self.rate_stop = self.create_rate(1/4)
-        self.th_front = self.th_side = 1.5
         self.cmd_pub = self.create_publisher(Twist, "/cmd_vel", 10) #Publisher
         self.ok = False
 
@@ -19,25 +18,47 @@ class Handler(Node):
                 self.cmd_pub.publish(self.msg)
 
     def rotate(self, clockwise=True):
-        rate = self.create_rate(1)
         factor = -1 if clockwise else 1
         msg = Twist()
         msg.angular.z = 1.57 * factor #rad/s
         self.msg = msg
         self.ok = True
-        rate.sleep()
+        a = self.get_clock().now().nanoseconds
+        while True:
+            b = self.get_clock().now().nanoseconds
+            diff = (b -a) * 1e-9
+            if diff> 1: 
+                break
         self.ok = False
 
     def go(self):
-        rate = self.create_rate(1/4)
         msg = Twist()
         msg.linear.x = 0.25 #m/s
         self.msg = msg
         self.ok = True
-        rate.sleep()
+        a = self.get_clock().now().nanoseconds
+        while True:
+            b = self.get_clock().now().nanoseconds
+            diff = (b -a) * 1e-9
+            if diff > 4.0: 
+                break
+        self.ok = False
+
+    def stop(self):
+        msg = Twist()
+        msg.linear.x = 0.0 #m/s
+        self.msg = msg
+        self.ok = True
+        a = self.get_clock().now().nanoseconds
+        while True:
+            b = self.get_clock().now().nanoseconds
+            diff = (b -a) * 1e-9
+            if diff > 1.0: 
+                break
         self.ok = False
     
     def loop(self):
+        self.get_clock().sleep_for(Duration(seconds=1))
         self.get_logger().info("Node starting...")
         #self.rate_stop.sleep()
         self.get_logger().info("Moving forward...")
@@ -48,6 +69,7 @@ class Handler(Node):
         #self.rate_stop.sleep()
         self.get_logger().info("Moving forward...")
         self.go()
+        self.stop()
         self.get_logger().info("End!")
 
 def main():
@@ -57,9 +79,12 @@ def main():
     param = rclpy.Parameter("use_sim_time", rclpy.Parameter.Type.BOOL, True)
     handller.set_parameters([param])
     executor.add_node(handller)
-    executor.create_task(handller.loop)
     executor.create_task(handller.move_callback)
-    executor.spin()
+    executor.create_task(handller.loop)
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
 
     handller.destroy_node()
     rclpy.shutdown()
